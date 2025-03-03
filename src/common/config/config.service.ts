c0e8ae671290607f23a3bd99f739a1ce06b97fce
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-03-03 16:00:28 -0300

    feat: made envConfigService static

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -2,12 +2,15 @@ export const Env = {
   FORUM_USERNAME: "FORUM_USERNAME",
   FORUM_PASSWORD: "FORUM_PASSWORD",
   FORUM_URL: "FORUM_URL",
+  PORT: "PORT",
+  NODE_ENV: "NODE_ENV",
+  AUTH_TOKEN: "AUTH_TOKEN",
 } as const;
 
 export type Env = (typeof Env)[keyof typeof Env];
 
 export class ConfigService {
-  getVar(env: Env) {
+  static getVar(env: Env) {
     const environment = process.env[env];
     if (!environment) {
       throw new Error(`Environment variable ${env} is not defined`);
