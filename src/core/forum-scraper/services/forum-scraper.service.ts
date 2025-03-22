db6a8ade9a3167a8c257e50aa13b3c735cde7469
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-03-22 04:01:27 -0300

    feat: implemented comment scraping by posts

--- src/core/forum-scraper/services/forum-scraper.service.ts
+++ src/core/forum-scraper/services/forum-scraper.service.ts
@@ -1,7 +1,7 @@
 import { Page } from "puppeteer";
 import { ConfigService, Env } from "../../../common/config/config.service";
 import { PuppeteerWrapper } from "../../../domain/puppeteer/puppeteer";
-import { Post } from "../../../domain/entities/post.entity";
+import { Comment, Post } from "../../../domain/entities/post.entity";
 
 export class ForumScraperService {
   constructor(private readonly puppeteer: PuppeteerWrapper) {}
@@ -18,6 +18,8 @@ export class ForumScraperService {
 
     await page.waitForNavigation({ waitUntil: "domcontentloaded" });
 
+    await page.goto(`${ConfigService.getVar(Env.FORUM_URL)}/search/?type=post`);
+
     await this.search(page, searchText);
 
     await page.waitForNavigation({ waitUntil: "domcontentloaded" });
@@ -29,72 +31,142 @@ export class ForumScraperService {
     return posts;
   }
 
-  async getPosts(page: Page) {
-    const result = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll("ol.block-body > li")).map(
-        (li) => {
-          const titleElement = li.querySelector<HTMLAnchorElement>(
-            ".contentRow-title a"
-          );
-          const repliesElement = Array.from(
-            li.querySelectorAll<HTMLLIElement>(".contentRow-minor li")
-          ).find((el) => el.innerText.includes("Replies:"));
-          const timeElement = li.querySelector<HTMLTimeElement>("time");
-          const snippetElement = li.querySelector<HTMLTimeElement>(
-            ".contentRow-snippet"
-          );
+  private async getPosts(page: Page) {
+    const posts = await this.getPostContent(page);
 
-          return {
-            title: titleElement?.innerText.trim() ?? "",
-            replies: repliesElement
-              ? parseInt(repliesElement.innerText.replace("Replies: ", ""), 10)
-              : 0,
-            date: timeElement?.dateTime ?? "",
-            author: li.getAttribute("data-author") ?? "",
-            url: titleElement?.href ?? "",
-            type: titleElement?.href?.includes("post-") ? "comment" : "post",
-            text: snippetElement?.innerText.trim() ?? "",
-          };
-        }
-      );
-    });
+    const PostWithComments: { post: Post; comments: Comment[] }[] = [];
 
-    const { posts, comments } = result.reduce(
-      (acc, result) => {
-        if (result.type === "post") {
-          acc.posts.push({
-            ...result,
-            evidences: [],
-          });
-        } else {
-          acc.comments.push(result);
-        }
+    for (const post of posts) {
+      await Promise.all([
+        page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+        page.goto(post.url),
+      ]);
 
-        return acc;
-      },
-      { posts: [], comments: [] } as any
-    );
+      const totalComments = await this.getComments(page);
 
-    for (const comment of comments) {
-      const post = posts.find((post: Post) => comment.url.startsWith(post.url));
-      if (post) {
-        const { replies, title, type, ...rest } = comment;
+      const totalPages = await this.getTotalPages(page);
 
-        post.evidences.push(rest);
+      for (let i = 2; i <= totalPages; i++) {
+        const postComments = await this.getComments(page);
+
+        totalComments.push(...postComments);
+
+        await Promise.all([
+          page.waitForNavigation({ waitUntil: "domcontentloaded" }),
+          page.goto(`${post.url}page-${i}`),
+        ]);
       }
+
+      PostWithComments.push({ post, comments: totalComments });
     }
 
-    return posts;
+    return PostWithComments;
   }
 
-  async search(page: Page, searchText: string) {
-    const searchXPath = `input[name="keywords"]`;
+  private async getPostContent(page: Page): Promise<Post[]> {
+    const postsData = await page.evaluate(() => {
+      const posts = Array.from(document.querySelectorAll("li.block-row"));
+
+      return posts.map((li) => {
+        const author = li.getAttribute("data-author") || "";
+        const titleAnchor = li.querySelector("h3.contentRow-title a");
+        const url = titleAnchor ? (titleAnchor as HTMLAnchorElement).href : "";
+        const title = titleAnchor ? titleAnchor.textContent?.trim() || "" : "";
+        const text =
+          li.querySelector("div.contentRow-snippet")?.textContent?.trim() || "";
+        const date = li.querySelector("time")?.getAttribute("datetime") || "";
+
+        let replies = 0;
+        const listItems = li.querySelectorAll("ul.listInline li");
+        listItems.forEach((item) => {
+          const itemText = item.textContent || "";
+          if (itemText.includes("Replies:")) {
+            const match = itemText.match(/Replies:\s*(\d+)/);
+            if (match && match[1]) {
+              replies = parseInt(match[1], 10);
+            }
+          }
+        });
+
+        let tags = "";
+        if (titleAnchor) {
+          const tagSpans = Array.from(
+            titleAnchor.querySelectorAll("span")
+          ).filter((span) => span.className.indexOf("prefix-") !== -1);
+          if (tagSpans.length) {
+            tags = `[${tagSpans
+              .map((span) => span.textContent?.trim())
+              .join("][")}]`;
+          }
+        }
+
+        return { author, url, date, replies, title, text, tags };
+      });
+    });
+
+    return postsData;
+  }
+
+  private async getTotalPages(page: Page) {
+    return await page.evaluate(() => {
+      const pageNav = document.querySelector(".pageNav-main");
+      if (!pageNav) return 1;
+      const lastPageLink = pageNav.querySelector("li:last-of-type a");
+      return lastPageLink ? parseInt(lastPageLink.textContent!.trim(), 10) : 1;
+    });
+  }
+
+  private async getComments(page: Page): Promise<Comment[]> {
+    const comments = await page.evaluate(() => {
+      const commentNodes = document.querySelectorAll(
+        `article[itemtype="https://schema.org/Comment"]`
+      );
+
+      return Array.from(commentNodes).map((node) => {
+        const authorElement = node.querySelector(".message-name a.username");
+        const dateElement = node.querySelector("time");
+        const contentElement = node.querySelector(".message-body");
+        const urlElement = node.querySelector("a.message-attribution-gadget");
+
+        return {
+          author: authorElement ? authorElement.textContent!.trim() : null,
+          date: dateElement ? dateElement.getAttribute("datetime") : null,
+          text: contentElement ? contentElement.textContent!.trim() : null,
+          url: urlElement ? (urlElement as HTMLAnchorElement).href : null,
+        };
+      });
+    });
+
+    return comments;
+  }
+
+  private async search(page: Page, searchText: string) {
+    const searchXPath = `input[class="input"]`;
 
     await this.puppeteer.waitAndType(page, searchXPath, searchText);
-    await this.puppeteer.focusAndPress(page, searchXPath, "Enter");
+
+    await page.evaluate(() => {
+      const displayResultsAsThreadsXPath = `input[name="grouped"]`;
+      const submitButtonXPath = `button[type="submit"]`;
+      const onlyPostsXPath = 'input[name="order"]';
+
+      const displayResultsAsThreads = document.querySelector(
+        displayResultsAsThreadsXPath
+      ) as HTMLInputElement;
+
+      const radios = document.querySelectorAll(onlyPostsXPath);
+
+      const submitButton = document.querySelector(
+        submitButtonXPath
+      ) as HTMLInputElement;
+
+      (radios[1] as any).click();
+      displayResultsAsThreads.click();
+      submitButton.click();
+    });
   }
 
-  async login(page: Page) {
+  private async login(page: Page) {
     const username = ConfigService.getVar(Env.FORUM_USERNAME);
     const password = ConfigService.getVar(Env.FORUM_PASSWORD);
 
