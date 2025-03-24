32ef9666f8791e6a6aa16c3b3e0abf2a4b07f4b4
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-03-24 19:28:34 -0300

    feat: add log method

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -2,3 +2,9 @@ export async function sleep(ms: number) {
   console.log("Waiting for " + ms + "ms");
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export async function log(message: string) {
+  if (process.argv.includes("verbose")) {
+    console.log(message);
+  }
+}
